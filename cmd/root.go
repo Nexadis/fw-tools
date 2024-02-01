5b6934cef2a533845c743c1be74a519c7b3f1609
Nexadis <37233902+Nexadis@users.noreply.github.com>
2024-02-01 09:02:58 +0300

    Swap command added

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -8,9 +7,11 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+
+	"github.com/Nexadis/fw-tools/internal/config"
 )
 
-
+var cfg config.Config
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -46,6 +47,5 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringVar(&cfg.Output, "o", "", "Output file for result. Stay empty if need auto generate")
 }
-
-
--- cmd/swap.go
+++ cmd/swap.go
@@ -4,16 +4,22 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
+	"errors"
+	"log"
 
 	"github.com/spf13/cobra"
+
+	"github.com/Nexadis/fw-tools/internal/config"
+	"github.com/Nexadis/fw-tools/internal/swap"
 )
+
+var swapCfg config.Swap
 
 // swapCmd represents the swap command
 var swapCmd = &cobra.Command{
-	Use:   "swap",
+	Use:   "swap [filename] [filename2]...",
 	Short: "Swap bits in byte, or bytes in word, or words in dword",
-	Long: `. For example:
+	Long: `Swap bits in byte, or bytes in word, or words in dword. For example:
 
 	1011 0110 -> 0110 1101 	# inverse bits
 	ABCD 			-> BADC 			# swap halfs
@@ -21,12 +27,35 @@ var swapCmd = &cobra.Command{
 	ABCD1234 	-> 1234ABCD		# swap words
 	ABCD1234567890EF -> 0x567890EFABCD1234 # swap dwords
 `,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("set filename")
+		}
+		cfg.Inputs = append(cfg.Inputs, args...)
+		return nil
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("swap called")
+		for _, input := range cfg.Inputs {
+			s := swap.Swapper{
+				Input:  input,
+				Output: cfg.Output,
+				Config: swapCfg,
+			}
+			err := s.Run()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
 	},
 }
 
 func init() {
+	swapCmd.Flags().BoolVarP(&swapCfg.Bits, "bits", "ib", false, "Inverse bits in byte")
+	swapCmd.Flags().BoolVarP(&swapCfg.Halfs, "halfs", "h", false, "Swap halfs of byte")
+	swapCmd.Flags().BoolVarP(&swapCfg.Bytes, "bytes", "sb", false, "Swap neighbors bytes")
+	swapCmd.Flags().BoolVarP(&swapCfg.Words, "words", "w", false, "Swap neighbors words")
+	swapCmd.Flags().BoolVarP(&swapCfg.Dwords, "dwords", "dw", false, "Swap neighbors dwords")
+
 	rootCmd.AddCommand(swapCmd)
 
 	// Here you will define your flags and configuration settings.
